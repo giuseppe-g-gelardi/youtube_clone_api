1ea06d9edaf271b3020072e6fa089aefd110ec78
n-rdy <giuseppegelardi@icloud.com>
2021-11-28 14:40:05 -0600

    change parameters of api key

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import { lightTheme, darkTheme } from './components/Theme/Theme';
 import Home from './pages/Home'
 import MainAppBar from './components/MainAppbar';
 
+import { API_KEY } from './API_KEY'
+
 export default function App() {
   const [darkMode, setDarkMode] = useState(false)
   const theme = (darkMode ? darkTheme : lightTheme)
@@ -15,6 +17,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
         <MainAppBar check={darkMode} change={() => setDarkMode(!darkMode)} />
+        <button onClick={() => console.log(API_KEY)}>api key logger</button>
           <Routes>
             <Route path='/' element={<Home />} />
           </Routes>
