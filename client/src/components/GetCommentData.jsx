b9c39cf55d5a756c43a5fad8b9f62e42852627b8
n-rdy <giuseppegelardi@icloud.com>
2021-11-29 13:21:19 -0600

    display related videos

--- client/src/components/GetCommentData.jsx
+++ client/src/components/GetCommentData.jsx
@@ -13,11 +13,12 @@ export default function GetCommentData() {
   // https://www.googleapis.com/youtube/v3/search?q=${userInput}&key=${key}&maxResults=10&order=viewCount&part=snippet
 
   // get related videos url
-  // https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoId}&type=video&key=${key}&part=snippet
+  // https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoid}&type=video&key=${key}&part=snippet
 
   const [comments, setComments] = useState([])
   const [replies, setReplies] = useState([])
   const [videoid, setVideoid] = useState('W0quDfpfRUQ') // Welcome to youtube - Bo burnham
+  const [relatedVideos, setRelatedVideos] = useState([])
 
   const hardcodecommentid = '619ecffbecdde10dac147d41'
 
@@ -56,6 +57,7 @@ export default function GetCommentData() {
   const getVideos = async () => {
     try {
       await axios.get(``).then((response => {
+        // get input from navbar search
         //set 
         //log
       }))
@@ -67,9 +69,10 @@ export default function GetCommentData() {
 
   const getRelatedVideos = async () => {
     try {
-      await axios.get(``).then((response) => {
-        // setRelatedVideos
-        // log 
+      await axios.get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoid}&type=video&key=${key}&part=snippet`)
+      .then((response) => {
+        console.log(response.data.items)
+        setRelatedVideos(response.data.items)
       })
     } catch (error) {
       console.log(error)
@@ -81,6 +84,7 @@ export default function GetCommentData() {
   useEffect(() => {
     getComments()
     // getReplies()
+    getRelatedVideos()
   }, [])
   // empty array for testing, add comments to array later
 
@@ -92,7 +96,7 @@ export default function GetCommentData() {
       />
 
       <RelatedVideos 
-        
+        relatedVideos={relatedVideos}
       />
 
 
@@ -100,7 +104,6 @@ export default function GetCommentData() {
         comments={comments}
         handleDelete={handleDelete}
         replies={replies}
-        // replies={replies}
       />
     </>
   )
--- client/src/components/RelatedVideos.jsx
+++ client/src/components/RelatedVideos.jsx
@@ -1,8 +1,44 @@
-export default function RelatedVideos() {
+import React, { useState, useEffect } from 'react'
+
+export default function RelatedVideos(props) {
+
+  const [related, setRelated] = useState([])
+
+  const { relatedVideos } = props
+
+  const getRelatedId = () => {
+    relatedVideos.map((videos) => {
+        setRelated(videos.id.videoId)
+    })
+  }
+
+  useEffect(() => {
+    getRelatedId()
+  })
   
   return (
-    <>
-      
-    </>
+    <div>
+    {/* <button onClick={() => console.log(relatedVideos)}>vids</button>
+    <button onClick={() => console.log(relatedVideos[0].id.videoId)}>id</button> */}
+
+      {relatedVideos.map((videos) => {
+        return (
+          <>
+          
+          <ul>
+            <iframe 
+              title='relatedvids'
+              width='160'
+              height='90'
+              src={`https://www.youtube.com/embed/${videos.id.videoId}?autoplay=0&origin=http://example.com`}
+              frameBorder='0'
+            />
+
+            
+          </ul>
+          </>
+        )
+      })}
+    </div>
   )
 }
