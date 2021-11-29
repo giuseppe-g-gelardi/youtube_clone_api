76d2611967aba30eb43c52561f95b07ab2a3391f
n-rdy <giuseppegelardi@icloud.com>
2021-11-29 10:52:04 -0600

    add scroll to hide to appbar

--- client/src/components/MainAppbar.jsx
+++ client/src/components/MainAppbar.jsx
@@ -4,21 +4,69 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import MuiSwitch from './controls/MuiSwitch';
+import { makeStyles } from '@material-ui/core'
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
+import Controls from './controls/Controls'
 
-export default function MainAppBar({ check, change }) {
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      width: '100%',
+      padding: theme.spacing(3),
+    },
+    root: {
+      display: 'flex',
+    },
+    title: {
+      flexGrow: 1,
+      padding: theme.spacing(2),
+    },
+    date: {
+      flexGrow: 1,
+    },
+    toolbar: theme.mixins.toolbar,
+  }
+})
+
+export default function MainAppBar(props) {
+
+  const { children, check, change } = props
+
+  const classes = useStyles()
+
+
+  function HideOnScroll(props) {
+    const { children, window } = props;
+  
+    const trigger = useScrollTrigger({
+      target: window ? window() : undefined,
+    });
+  
+    return (
+      <Slide appear={false} direction="down" in={!trigger}>
+        {children}
+      </Slide>
+    );
+  }
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" elevation={10} color='transparent'>
-        <Toolbar>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            Welcome to the Tubeee
-          </Typography>
-          {/* dark mode toggle switch */}
-          <MuiSwitch onChange={change} checked={check} />
-          {/* dark mode toggle */}
-        </Toolbar>
-      </AppBar>
+      <HideOnScroll {...props}>
+        <AppBar position="fixed" elevation={10} color='transparent'>
+          <Toolbar>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              Welcome to the Tubeee
+            </Typography>
+            <MuiSwitch onChange={change} checked={check} />
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+        <div className={classes.page}>
+          <div classname={classes.toolbar}>
+        </div>
+          {children}
+        </div>
     </Box>
   );
 }
